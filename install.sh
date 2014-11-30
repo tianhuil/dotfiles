eb41080d88dc564d3a884c9cf76dcb543a08f87a
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2014-11-30 13:47:34 -0500

    updating install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
-sudo apt-get upgrade
-sudo apt-get ruby
+sudo apt-get upgrade --fix-missing
+sudo apt-get ruby --fix-missing
 
 # setup virtual env
-sudo apt-get install python-pip
+sudo apt-get install python-pip --fix-missing
 pip install virtualenv
 cd ~
 virtualenv venv
 . venv/bin/activate
 
+# setup python
+sudo apt-get install ipython --fix-missing
+
 # setup tmux
-sudo apt-get install tmux
+sudo apt-get install tmux --fix-missing
 gem install tmuxinator
 
 # setup emacs
-sudo apt-get install emacs
+sudo apt-get install emacs --fix-missing
