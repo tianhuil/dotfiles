b125824b56c906dfe169a11d158f3e5aa1dc967c
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2016-07-12 14:19:08 -0400

    Adding tmux conf

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ cp .inputrc ~/.
 cp .gitignore_global ~/.
 git config --global core.excludesfile ~/.gitignore_global
 cp .gitconfig ~/.
+cp .tmux.conf ~/.
 cp git-completion.sh ~/.git-completion.sh
 cp git-prompt.sh ~/.git-prompt.sh
