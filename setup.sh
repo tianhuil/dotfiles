8af62c0c872a4efc3e1a3e9316a3e32aecd25008
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2016-07-12 14:09:06 -0400

    Adding gitignore global

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 cp .bashrc ~/.
 cp .inputrc ~/.
+cp .gitignore_global ~/.
+git config --global core.excludesfile ~/.gitignore_global
 cp .gitconfig ~/.
 cp git-completion.sh ~/.git-completion.sh
 cp git-prompt.sh ~/.git-prompt.sh
