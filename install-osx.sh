d0da4981e71e20f9f9dac7d5e889041007d82340
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2016-07-16 11:24:46 -0400

    Adding more commands

--- install-osx.sh
+++ install-osx.sh
@@ -1,10 +1,28 @@
 # install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Software to download
+# Dropbox
+# 1password (password file)
+# SizeUp (license)
+# Chrome
+# Sublime
+# Slack
+
+# tmux
 brew install tmux
 sudo gem install tmuxinator
 brew install homebrew/completions/tmuxinator-completion
 
+# setup sublime
 if [ -f "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ]; then
 	ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
 fi
+
+# install java
+brew tap caskroom/cask
+brew install brew-cask
+brew cask install java
+
+# install spark
+brew install apache-spark
