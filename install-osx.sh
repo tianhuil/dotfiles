c356768f1686029feeb0a54a45c9bf6a173d7855
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-06-02 13:08:21 -0400

    Update installation script for OSX

--- install-osx.sh
+++ install-osx.sh
@@ -1,4 +1,4 @@
-# install brew
+# Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Software to download
@@ -28,16 +28,16 @@ fi
 # http://stackoverflow.com/questions/25718021/sublime-text-no-longer-launches-from-terminal
 brew install reattach-to-user-namespace
 
-# install java
+# Install java
 brew tap caskroom/cask
 brew install brew-cask
 brew cask install java
 
-# install spark
+# Install spark
 brew install scala
 brew install apache-spark
 
-# install GCloud
+# Install GCloud
 brew cask install google-cloud-sdk
 echo "Be sure to symlink gcloud"
 echo "sudo ln -s /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/gcloud /usr/local/bin/gcloud"
@@ -45,13 +45,13 @@ echo "sudo ln -s /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bi
 # gitx
 ln -s /Applications/GitX.app/Contents/Resources/gitx /usr/local/bin/gitx
 
-# instal google app engine
+# Install google app engine
 brew install google-app-engine
 
 # tidy
 brew install tidy-html5
 
-# gpg
+# Install gpg
 brew install gpg
 
 # parallel
@@ -62,8 +62,14 @@ brew install bash-completion
 
 xcode-select --install
 
-# Latex
+# Install Latex
 brew cask install mactex
 
 # Install go
 brew install go
+
+# Install node and npm from https://gist.github.com/DanHerbert/9520689
+rm -rf /usr/local/lib/node_modules
+brew uninstall node
+brew install node --without-npm
+curl -L https://www.npmjs.com/install.sh | sh
