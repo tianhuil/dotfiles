7a35c9b19b7973ed0170f6496267190545a3f9b9
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-08-22 22:30:54 -0400

    Adding diff.sh

---
+++ diff.sh
@@ -0,0 +1,8 @@
+diff .bashrc ~/.bashrc
+diff .inputrc ~/.inputrc
+diff .npmrc ~/.npmrc
+diff .gitignore_global ~/.gitignore_global
+diff .gitconfig ~/.gitconfig
+diff .tmux.conf ~/.tmux.conf
+diff git-completion.sh ~/.git-completion.sh
+diff git-prompt.sh ~/.git-prompt.sh
