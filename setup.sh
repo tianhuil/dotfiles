82000161ee9f18f8872deecf40ce722b9de3265b
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-08-22 22:31:18 -0400

    git grep blame

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 cp .bashrc ~/.
 cp .inputrc ~/.
 cp .npmrc ~/.
-cp .gitignore_global ~/.
-git config --global core.excludesfile ~/.gitignore_global
 cp .gitconfig ~/.
 cp .tmux.conf ~/.
 cp git-completion.sh ~/.git-completion.sh
 cp git-prompt.sh ~/.git-prompt.sh
 cp -R .scripts ~/.
-chmod u+x ~/*
+cp .gitignore_global ~/.
+git config --global core.excludesfile ~/.gitignore_global
+chmod u+x ~/.*
