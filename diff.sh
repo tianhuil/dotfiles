dd8431ae165338b8eda7a9c0ff47dfaf00a5624b
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-09-09 18:53:46 -0400

    echo in diff.sh

--- diff.sh
+++ diff.sh
@@ -1,8 +1,16 @@
+echo .bashrc
 diff .bashrc ~/.bashrc
+echo .inputrc
 diff .inputrc ~/.inputrc
+echo .npmrc
 diff .npmrc ~/.npmrc
+echo .gitignore_global
 diff .gitignore_global ~/.gitignore_global
+echo .gitconfig
 diff .gitconfig ~/.gitconfig
+echo .tmux.conf
 diff .tmux.conf ~/.tmux.conf
+echo git-completion.sh
 diff git-completion.sh ~/.git-completion.sh
+echo git-prompt.sh
 diff git-prompt.sh ~/.git-prompt.sh
