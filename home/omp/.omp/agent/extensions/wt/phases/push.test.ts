26e6a2eb585f77e3615f500babc149d92bb15478
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2026-06-29 23:05:36 -0400

    Revert "Rebase worktree onto origin/${baseRef} before push, check for commits ahead, better PR error messages"
    This reverts commit 98dd8291b9c41230a2b1f8072340891a47465a09.

--- home/omp/.omp/agent/extensions/wt/phases/push.test.ts
+++ home/omp/.omp/agent/extensions/wt/phases/push.test.ts
@@ -89,8 +89,6 @@ describe("phasePushPR", () => {
     const exec = makeFakeGhExec({
       "git remote get-url origin": { stdout: "git@github.com:user/repo.git" },
       "git fetch origin": { stdout: "" },
-      "git rebase origin": { stdout: "Successfully rebased" },
-      "git rev-list --count origin": { stdout: "1" },
       "git push -u origin": { stdout: "Everything up-to-date" },
       "gh pr create": {
         stdout: "https://github.com/user/repo/pull/42",
@@ -125,9 +123,8 @@ describe("phasePushPR", () => {
   test("throws PUSH_AUTH_FAILURE on permission error", async () => {
     const exec = makeFakeGhExec({
       "git remote get-url origin": { stdout: "git@github.com:user/repo.git" },
+
       "git fetch origin": { stdout: "" },
-      "git rebase origin": { stdout: "Successfully rebased" },
-      "git rev-list --count origin": { stdout: "1" },
       "git push -u origin": {
         stdout: "",
         stderr:
@@ -144,12 +141,9 @@ describe("phasePushPR", () => {
   });
 
   test("throws PUSH_FAILED on non-auth push error", async () => {
-
     const exec = makeFakeGhExec({
       "git remote get-url origin": { stdout: "git@github.com:user/repo.git" },
       "git fetch origin": { stdout: "" },
-      "git rebase origin": { stdout: "Successfully rebased" },
-      "git rev-list --count origin": { stdout: "1" },
       "git push -u origin": {
         stdout: "",
         stderr: "src refspec does not match any",
@@ -163,12 +157,10 @@ describe("phasePushPR", () => {
   });
 
   test("throws PR_CREATE_FAILED when gh pr create errors", async () => {
-
     const exec = makeFakeGhExec({
       "git remote get-url origin": { stdout: "git@github.com:user/repo.git" },
+
       "git fetch origin": { stdout: "" },
-      "git rebase origin": { stdout: "Successfully rebased" },
-      "git rev-list --count origin": { stdout: "1" },
       "git push -u origin": { stdout: "Everything up-to-date" },
       "gh pr create": {
         stdout: "",
@@ -178,7 +170,6 @@ describe("phasePushPR", () => {
     });
     const state = makeState();
     const { io } = makeTestIo();
-
 
     await expect(phasePushPR(exec, state, io)).rejects.toThrow(
       "PR_CREATE_FAILED",
